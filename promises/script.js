9d8ad487429ce82f1bcebae281ea445c4e26d49b
EngelsAS <engelsantero@gmail.com>
2024-11-16 16:57:44 -0300

    ADD: atividade Promises

---
+++ promises/script.js
@@ -0,0 +1,30 @@
+function preencherValores(valores) {
+  document.getElementById("cep").textContent = `CEP: ${valores.cep}`;
+  document.getElementById(
+    "logradouro"
+  ).textContent = `Logradouro: ${valores.logradouro}`;
+  document.getElementById(
+    "estado"
+  ).textContent = `Estado: ${valores.estado} - ${valores.uf}`;
+  document.getElementById("bairro").textContent = `Bairro: ${valores.bairro}`;
+  document.getElementById(
+    "localidade"
+  ).textContent = `Localidade: ${valores.localidade}`;
+}
+
+async function getCep() {
+  const cepInput = document.getElementById("cep_input");
+  const cepValue = cepInput.value;
+
+  try {
+    console.log(cepValue);
+    const resp = await fetch(`https://viacep.com.br/ws/${cepValue}/json/`);
+    const data = await resp.json();
+
+    console.log(data);
+
+    preencherValores(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
